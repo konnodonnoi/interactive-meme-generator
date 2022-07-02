90e0bf660f30db3bae3bb6e86c96a286ddfc8035
konnodonnoi <saboanamero8521@gmail.com>
2022-07-01 16:54:45 -1200

    commits

--- src/components/memes/Memes.js
+++ src/components/memes/Memes.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from 'react';
+import memesData from '../../memesData';
 import './Memes.css';
 
 const Memes = () => {
+
+let url 
+  const getMemeImage = () => {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+     url = memesArray[randomNumber].url
+    console.log(url)
+  }
+    
+  
   return (
   <main>
-    <form className='form'>
+
+    <p>{url}</p>
+    <div className='form'>
+
         <input type='text' className='form--input' placeholder='top-text'/>
         <input type='text' className='form--input' placeholder='bottom-text'/>
-        <button className='form--button'>get a new meme image 🖼</button>
-    </form>
+        <button className='form--button' onClick={getMemeImage} >get a new meme image 🖼</button>
+    </div>
   </main>
   )
 }

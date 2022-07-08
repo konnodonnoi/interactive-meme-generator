2bd12c25764a37bff67adcd4cc03502444777294
me <saboanamero8521@gmail.com>
2022-07-07 13:26:37 -1200

    commits

--- src/components/memes/Memes.js
+++ src/components/memes/Memes.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import memesData from '../../memesData';
-import './Memes.css';
+import React, { useState } from "react";
+import memesData from "../../memesData";
+import "./Memes.css";
 
 const Memes = () => {
+  const [memeImage, setImage] = useState("")
 
-let url 
-  const getMemeImage = () => {
-    const memesArray = memesData.data.memes
-    const randomNumber = Math.floor(Math.random() * memesArray.length)
-     url = memesArray[randomNumber].url
-    console.log(url)
-  }
-    
   
+  let url;
+  const getMemeImage = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setImage(url = memesArray[randomNumber].url)
+    // url = memesArray[randomNumber].url;
+    // console.log(url);
+  };
+
   return (
-  <main>
-
-    <p>{url}</p>
-    <div className='form'>
-
-        <input type='text' className='form--input' placeholder='top-text'/>
-        <input type='text' className='form--input' placeholder='bottom-text'/>
-        <button className='form--button' onClick={getMemeImage} >get a new meme image 🖼</button>
-    </div>
-  </main>
-  )
-}
+    <main>
+      <p>{url}</p>
+      <div className="form">
+        <input type="text" className="form--input" placeholder="top-text" />
+        <input type="text" className="form--input" placeholder="bottom-text" />
+        <button className="form--button" onClick={getMemeImage}>
+          get a new meme image 🖼
+        </button>
+      </div>
+      <img src={memeImage} alt="random meme imagery"/>
+    </main>
+  );
+};
 
 export default Memes;
